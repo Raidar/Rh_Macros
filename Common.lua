7bd86fec86186e42c9aca16ac50a73144fd6f581
Aidar Rakhmatullin <raidar@yandex.ru>
2013-01-06 13:58:52 +0400

    Common macros

---
+++ Common.lua
@@ -0,0 +1,60 @@
+--[[ Common macros ]]--
+
+----------------------------------------
+--[[ description:
+  -- Macros for common areas.
+  -- Макросы для общих областей.
+--]]
+----------------------------------------
+--[[ uses:
+  nil.
+  -- group: Macros.
+--]]
+--------------------------------------------------------------------------------
+local Macro = Macro
+if not Macro then
+  Macro = function () end
+end
+
+---------------------------------------- Escape
+Macro {
+  area = "Shell Info QView Tree",
+  key = "Esc",
+  flags = "DisableOutput",
+  description = "All: Esc - Panels on/off",
+  action = function ()
+             Keys"CtrlO"
+           end,
+} ---
+---------------------------------------- XLat
+Macro {
+  area = "Shell Info QView Tree Editor",
+  key = "CtrlAltX",
+  flags = "DisableOutput",
+  description = "All: XLat",
+  action = function ()
+             Keys"XLat"
+           end,
+} ---
+---------------------------------------- File name
+-- "'" - Active Panel (changed from used "F").
+-- ";" - Passive Panel (used by default configs).
+Macro {
+  area = "Shell Info QView Tree",
+  key = "Ctrl'",
+  flags = "DisableOutput",
+  description = "All: Full name on AP",
+  action = function ()
+             Keys"CtrlF"
+           end,
+} ---
+Macro {
+  area = "Shell Info QView Tree",
+  key = "CtrlAlt'",
+  flags = "DisableOutput",
+  description = "All: Network name on AP",
+  action = function ()
+             Keys"CtrlAltF"
+           end,
+} ---
+--------------------------------------------------------------------------------
