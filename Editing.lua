ee252d3716b50d2657b0e6848771a74bbb4da166
Aidar Rakhmatullin <raidar@yandex.ru>
2013-01-08 20:58:52 +0400

    Macros for Editor with Plugins

---
+++ Editing.lua
@@ -0,0 +1,89 @@
+--[[ Editing macros ]]--
+
+----------------------------------------
+--[[ description:
+  -- Macros for text editing.
+  -- Макросы для правки текста.
+--]]
+----------------------------------------
+--[[ uses:
+  nil.
+  -- group: Macros/Plugins.
+--]]
+--------------------------------------------------------------------------------
+
+----------------------------------------
+local far = far
+local F = far.Flags
+
+local BlockNoneType = F.BTYPE_NONE
+
+local editor = editor
+
+----------------------------------------
+--[[
+local dbg = require "context.utils.useDebugs"
+local logShow = dbg.Show
+--]]
+
+--------------------------------------------------------------------------------
+--local unit = {}
+
+local guids, guid = {}
+--unit.guids = guids
+
+local Macro = Macro or function () end
+
+local Async = function () return mmode(3, 1) end
+
+local PluginExist = Plugin.Exist
+local PluginMenu, CallPlugin = Plugin.Menu, Plugin.Call
+
+---------------------------------------- 'R' -- EditCase
+guid = "0E92FC81-4888-4297-A85D-31C79E0E0CEE"
+guids.EditCase = guid
+
+local Exist = function () return PluginExist(guid) end
+
+-- [[
+Macro {
+  area = "Editor",
+  key = "F5",
+  flags = "DisableOutput",
+  description = "Case: Next case",
+  condition = Exist,
+  action = function ()
+             return CallPlugin(guid, 4)
+           end, ---
+} ---
+
+Macro {
+  area = "Editor",
+  key = "CtrlShiftF5",
+  flags = "DisableOutput",
+  description = "Case: Sentence case for Bulleted line",
+  condition = Exist,
+  action = function ()
+             local Info = editor.GetInfo()
+             Keys"ShiftHome CtrlShiftRight CtrlShiftLeft ShiftRight"
+             CallPlugin(guid, 0)
+             editor.Select(Info.EditorID, BlockNoneType)
+             return editor.SetPosition(Info.EditorID, -1, Info.CurPos)
+           end, ---
+} ---
+Macro {
+  area = "Editor",
+  key = "CtrlAltShiftF5",
+  flags = "DisableOutput",
+  description = "Case: Sentence case for Numbered line",
+  condition = Exist,
+  action = function ()
+             local Info = editor.GetInfo()
+             Keys"ShiftHome CtrlShiftRight CtrlShiftRight CtrlShiftLeft ShiftRight"
+             CallPlugin(guid, 0)
+             editor.Select(Info.EditorID, BlockNoneType)
+             return editor.SetPosition(Info.EditorID, -1, Info.CurPos)
+           end, ---
+} ---
+--]]
+--------------------------------------------------------------------------------
