7043bac7ab6f7a801b1bc5616724e1c9e22f49b4
Aidar Rakhmatullin <raidar@yandex.ru>
2013-01-20 19:22:08 +0400

    Macros to use LuaFAR Search

--- Plugins.Search.lua
+++ Plugins.Search.lua
@@ -30,14 +30,57 @@ local guids = {}
 --unit.guids = guids
 
 local Macro = Macro or function () end
+local Plugin = Plugin or {}
 
 local Async = function () return mmode(3, 1) end
 
 local PluginExist = Plugin.Exist
 local PluginMenu, CallPlugin = Plugin.Menu, Plugin.Call
 
----------------------------------------- Shell
+---------------------------------------- Common
 
+---------------------------------------- 'F' -- LuaFAR Search
+-- [[
+guids.LFSearch = "8E11EA75-0303-4374-AC60-D1E38F865449"
+
+local Exist = function () return PluginExist(guids.LFSearch) end
+
+Macro {
+  area = "Shell Editor",
+  key = "CtrlShiftF",
+  flags = "DisableOutput",
+  description = "SnR: Find",
+  condition = Exist,
+  action = function ()
+    --return CallPlugin(guids.LFSearch, 1)
+    if PluginMenu(guids.LFSearch) then return Keys"1" end
+    -- TODO: Добавить подстановку из предыдущего поиска.
+  end, ---
+} ---
+Macro {
+  area = "Editor",
+  key = "CtrlShiftR",
+  flags = "DisableOutput",
+  description = "SnR: Replace",
+  condition = Exist,
+  action = function ()
+    --return CallPlugin(guids.LFSearch, 2)
+    if PluginMenu(guids.LFSearch) then return Keys"2" end
+    -- TODO: Добавить подстановку из предыдущего поиска.
+  end, ---
+} ---
+Macro {
+  area = "Editor",
+  key = "ShiftF3",
+  flags = "DisableOutput",
+  description = "SnR: Repeat",
+  condition = Exist,
+  action = function ()
+    --return CallPlugin(guids.LFSearch, 3)
+    if PluginMenu(guids.LFSearch) then return Keys"3" end
+  end, ---
+} ---
+--]]
 ---------------------------------------- Editor
 
 ---------------------------------------- '(' -- Brackets
